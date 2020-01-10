15d3ff67b6123abe0ea3283c22692f829cc8a7c4
Dylan Bolter <bolterd01@student.psdr3.org>
2020-01-10 12:23:42 -0600

    show a second screen that displays password

--- Unit2_1/activity214.py
+++ Unit2_1/activity214.py
@@ -4,13 +4,18 @@
 ##############################################################################
 import tkinter as tk
 
+def test_my_button():
+    frame_auth.tkraise()
+    password = ent_pass.get()
+    show_pass.config(text=password, font='Arial')
+
 # main window
 root = tk.Tk()
 root.wm_geometry("300x200")
 root.title('Authorization')
 
 frame_login = tk.Frame(root)
-frame_login.grid()
+frame_login.grid(row=0, column=0, sticky="news")
 
 lbl_username = tk.Label(frame_login, text='Username:')
 lbl_username.pack()
@@ -22,7 +27,14 @@ pass_lbl.pack()
 ent_pass = tk.Entry(frame_login, bd=3, show='*')
 ent_pass.pack(pady=5)
 
-btn_login = tk.Button(frame_login, text="Login")
+btn_login = tk.Button(frame_login, text="Login", command=test_my_button)
 btn_login.pack(pady=5)
 
+frame_auth = tk.Frame(root)
+
+frame_auth.grid(row=0, column=0, sticky="news")
+show_pass = tk.Label(frame_auth, text='sup chief')
+show_pass.pack()
+
+frame_login.tkraise()
 root.mainloop()
