fe56ad64affb220fe5277f081ebf07868b50f276
Dylan Bolter <bolterd01@student.psdr3.org>
2020-02-23 18:45:12 -0600

    started to make the graph prog for the light

--- Unit3_1/light.py
+++ Unit3_1/light.py
@@ -4,3 +4,23 @@ path = 'data_files/light.csv'
 
 with open(path) as f:
     data = f.read().split('\n')
+
+fig,ax = plt.subplots(1,3)
+
+time = []
+nm615 = []
+nm525 = []
+nm465 = []
+
+for item in data[1:]:
+    num = item.split(',')
+    current_time = float(num[0])
+    time.append(current_time)
+    nm615.append(num[1])
+    nm525.append(num[2])
+    nm465.append(num[3])
+
+ax[0][0].plot(time, nm525)
+plt.plot(time, nm465)
+plt.yticks(range(0,40,1))
+plt.show()
