d03ebe1b86011e9681f922179aec0cc97c184cc4
Dylan Bolter <bolterd01@student.psdr3.org>
2020-02-26 12:22:34 -0600

    did some shenanigans

--- Unit3_1/vernier.py
+++ Unit3_1/vernier.py
@@ -6,11 +6,17 @@ gdx.open_usb()
 gdx.select_sensors([2,3])
 gdx.start()
 
-for i in range(40):
+sound_a, sound_c = [], []
+
+for i in range(5):
     measure = gdx.read()
     if measure == None:
         break
-    print(measure)
+    sound_a.append(float(measure[0]))
+    sound_c.append(float(measure[1]))
+
+print(sound_a)
+print(sound_c)
 
 gdx.stop()
 gdx.close()
