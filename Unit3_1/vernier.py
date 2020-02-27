91cb7632251208e7d3cfcb60c5a6af2676ab9493
Dylan Bolter <bolterd01@student.psdr3.org>
2020-02-27 10:37:52 -0600

    made a time list which makes code more readable

--- Unit3_1/vernier.py
+++ Unit3_1/vernier.py
@@ -6,7 +6,8 @@ gdx.open_usb()
 gdx.select_sensors([2,3])
 gdx.start()
 
-sound_a, sound_c = [], []
+sound_a, sound_c, time = [], [], []
+count = 0
 
 for i in range(5):
     measure = gdx.read()
@@ -14,16 +15,16 @@ for i in range(5):
         break
     sound_a.append(float(measure[0]))
     sound_c.append(float(measure[1]))
-
-print(sound_a)
-print(sound_c)
+    time.append(count)
+    count += 1
 
 gdx.stop()
 gdx.close()
 
+
 #prewrite(no testing available)might not work
 #plots this data on one graph
-ply.plot(len(sound_a), sound_a)#graphs the a sounds
-ply.plot(len(sound_a), sound_c)#graphs the c sounds
+ply.plot(time, sound_a)#graphs the a sounds
+ply.plot(time, sound_c)#graphs the c sounds
 plt.title(label='Vernier')
 plt.show()
