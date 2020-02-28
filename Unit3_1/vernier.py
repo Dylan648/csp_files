e9933da36ee7588c10808425e53965375b81fd41
Dylan Bolter <bolterd01@student.psdr3.org>
2020-02-28 12:36:19 -0600

    animated it(kinda)

--- Unit3_1/vernier.py
+++ Unit3_1/vernier.py
@@ -2,29 +2,36 @@ import gdx, matplotlib.pyplot as plt
 
 gdx = gdx.gdx()
 
+samples = int(input('How many samples would you like to take?: '))
+
 gdx.open_usb()
 gdx.select_sensors([2,3])
-gdx.start()
+gdx.start(period=100)
 
 sound_a, sound_c, time = [], [], []
 count = 0
 
-for i in range(5):
+for i in range(samples):
     measure = gdx.read()
-    if measure == None:
-        break
-    sound_a.append(float(measure[0]))
-    sound_c.append(float(measure[1]))
+    sound_a.append(measure[0])
+    sound_c.append(measure[1])
     time.append(count)
     count += 1
+    plt.ion()
+    plt.gcf()
+    plt.plot(sound_a, color='red')
+    plt.plot(sound_c, color='green')
+    plt.draw()
+    plt.pause(.05)
 
-gdx.stop()
-gdx.close()
+    if measure == None:
+        break
 
 
-#prewrite(no testing available)might not work
 #plots this data on one graph
-ply.plot(time, sound_a)#graphs the a sounds
-ply.plot(time, sound_c)#graphs the c sounds
+plt.plot(time, sound_a)#graphs the a sounds
+plt.plot(time, sound_c)#graphs the c sounds
 plt.title(label='Vernier')
 plt.show()
+gdx.stop()
+gdx.close()
