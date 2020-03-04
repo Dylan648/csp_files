dfdba6d251e7a2bc6cba87ad9e85f315811e64e6
Dylan Bolter <bolterd01@student.psdr3.org>
2020-03-04 12:35:42 -0600

    finished sensor gui

--- Unit3_1/sensor_gui.py
+++ Unit3_1/sensor_gui.py
@@ -1,9 +1,9 @@
 from tkinter import *
 import gdx, matplotlib.pyplot as plt, numpy as np
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 gdx = gdx.gdx()
 gdx.open_usb()
-gdx.select_sensors([2,3])
 
 sound_a, sound_c = [], []
 def start_reading():
@@ -14,6 +14,7 @@ def start_reading():
     gdx.start(rate)
     interval = 1 / samples
     length = np.arange(0, seconds, interval)
+    ax.cla()
     for i in range(samples * seconds):
         measure = gdx.read()
         sound_a.append(measure[0])
@@ -22,27 +23,42 @@ def start_reading():
             break
 
     plt.yscale('linear')
-    plt.plot(length, sound_a, c='red')
-    plt.plot(length, sound_c, c='blue')
+    ax.plot(length, sound_a, c='red')
+    ax.plot(length, sound_c, c='blue')
+    plt.ylabel(ylabel='Sound(decibels)')
+    plt.xlabel(xlabel='Time(seconds)')
     plt.show()
     gdx.stop()
     
-
-
-
 root = Tk()
 
 seconds_label = Label(root, text='How many seconds?')
-seconds_label.pack()
+seconds_label.grid(row=0, column=0)
 seconds_field = Entry(root)
-seconds_field.pack()
+seconds_field.grid(row=1,column=0)
 
 samples_label = Label(root, text='How many samples / seconds?')
-samples_label.pack()
+samples_label.grid(row=2, column=0)
 samples_field = Entry(root)
-samples_field.pack()
+samples_field.grid(row=3, column=0)
+
+# sensor_label = Label(root, text='What sensor(s) would you like to use?')
+# sensor_label.pack()
+# sensor_field = Entry(root)
+# sensor_field.pack()
+
+# sensor_list = []
+# for choice in sensor_field.get():
+#     num = choice.split(' ')
+#     sensor_list.append(num)
+gdx.select_sensors([2,3])
 
 start_button = Button(root, text='Start Sensing', command=start_reading)
-start_button.pack()
+start_button.grid(row=4, column=0)
+
+fig, ax = plt.subplots(1,1)
+plt.ion()
+chart = FigureCanvasTkAgg(fig, root)
+chart.get_tk_widget().grid(row=0, column=1, rowspan=20)
 
 root.mainloop()
