247efc45ce8b65faff236e8ebe2cebbaca4b16a0
Dylan Bolter <bolterd01@student.psdr3.org>
2020-03-09 12:22:05 -0500

    created game file and got player sprite and move

--- project/project.py
+++ project/project.py
@@ -1,8 +1,41 @@
-from tkinter import *
+import turtle, tkinter as tk
 
-root = Tk()
+screen = turtle.Screen()
+jump = False
 
-canvas = Canvas(root, width=1600, height=800)
-canvas.pack()
 
-root.mainloop()
+def handle_jump():
+    dino.forward(200)
+    dino.back(200)
+
+sprite = 'sprites/dino.gif'
+screen.addshape(sprite)
+# obs = 'sprites/log.gif'
+# screen.addshape(obs)
+dino = turtle.Turtle(sprite)
+
+ground = turtle.Turtle()
+ground.hideturtle()
+
+obstacle = turtle.Turtle()
+ground.speed(0)
+dino.shapesize(1)
+dino.penup()
+dino.goto(-150, 0)
+dino.left(90)
+
+ground.penup()
+ground.goto(-600,-100)
+ground.pendown()
+ground.forward(1000)
+
+obstacle.penup()
+obstacle.goto(300,0)
+
+screen.onkeypress(handle_jump, 'space')
+
+while obstacle.xcor() != dino.xcor():
+    obstacle.back(10)
+
+screen.listen()
+screen.mainloop()
