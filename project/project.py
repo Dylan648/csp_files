f858edb35735a51e31e778f95152c31d6ea07771
Dylan Bolter <bolterd01@student.psdr3.org>
2020-03-11 12:16:51 -0500

    completed game, need to get grip input setup

--- project/project.py
+++ project/project.py
@@ -1,9 +1,10 @@
-import turtle, tkinter as tk
+import turtle, tkinter as tk, random
 
 screen = turtle.Screen()
 game_over = False
 jump = False
 score = 0
+game_over = False
 
 def handle_jump():
     dino.forward(220)
@@ -17,6 +18,8 @@ def move_enemy():
         obstacle.back(10)
     if not game_over:
         screen.ontimer(move_enemy, 5)
+    else:
+        obstacle.back(5)
 
 def respawn_enemy():
     if obstacle.xcor() == -500:
@@ -24,6 +27,56 @@ def respawn_enemy():
         obstacle.goto(300,-75)
         obstacle.showturtle()
     screen.ontimer(respawn_enemy, 5)
+
+def count_change():
+    global score
+    global game_over
+    if not game_over:
+        if obstacle.xcor() == dino.xcor() - 10:
+            score += 1
+            counter.goto(0,250)
+            counter.setheading(0)
+            counter.clear()
+            counter.write(score, align='right', font=('Ariel', 20, 'normal'))
+        screen.ontimer(count_change, 25)
+
+def check_collision():
+    global score
+    global game_over
+    if dino.ycor() == obstacle.ycor() and dino.xcor() == obstacle.xcor():
+        game_over = True
+        score = 0
+        counter.goto(0,250)
+        counter.setheading(0)
+        counter.clear()
+        counter.write(score, align='right', font=('Ariel', 20, 'normal'))
+        restarter.penup()
+        restarter.goto(0, 100)
+        restarter.setheading(0)
+        restarter.write('GAME OVER, Press r to restart the game', align='center', font=('Ariel', 30, 'normal'))
+    screen.ontimer(check_collision, 15)
+
+def restart():
+    global game_over
+    if game_over:
+        game_over = False
+        restarter.clear()
+        start_game()
+
+def start_game():
+    screen.onkeypress(handle_jump, 'space')
+    move_enemy()
+    respawn_enemy()
+    check_collision()
+    count_change()
+    screen.onkeypress(restart, 'r')
+    obstacle.penup()
+    obstacle.hideturtle()
+    obstacle.goto(300,-75)
+    obstacle.showturtle()
+
+    screen.listen()
+    screen.mainloop()
 
 sprite = 'sprites/dino.gif'
 screen.addshape(sprite)
@@ -33,10 +86,15 @@ dino = turtle.Turtle(sprite)
 
 ground = turtle.Turtle()
 ground.hideturtle()
+stationary_score = turtle.Turtle()
+stationary_score.hideturtle()
 counter = turtle.Turtle()
 counter.hideturtle()
+restarter = turtle.Turtle()
+restarter.hideturtle()
 obstacle = turtle.Turtle()
 obstacle.shape('turtle')
+obstacle.hideturtle()
 ground.speed(0)
 dino.shapesize(1)
 dino.penup()
@@ -50,18 +108,14 @@ ground.pendown()
 ground.forward(1000)
 
 counter.penup()
-counter.left(90)
-counter.forward(250)
-counter.right(90)
-counter.write('Score = ', align='left', font=('Ariel', 20, 'normal'))
-counter.forward(100)
-counter.write(score, align='right', font=('Ariel', 20, 'normal'))
+stationary_score.penup()
+stationary_score.goto(-100, 250)
+stationary_score.setheading(0)
+stationary_score.write('Score = ', align='left', font=('Ariel', 20, 'normal'))
+
+
 obstacle.penup()
 obstacle.goto(300,-75)
+obstacle.showturtle()
 
-screen.onkeypress(handle_jump, 'space')
-move_enemy()
-respawn_enemy()
-
-screen.listen()
-screen.mainloop()
+start_game()
